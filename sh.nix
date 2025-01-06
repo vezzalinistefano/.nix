5e4d81b13b777077250096a252414306fe1eb861
vezzalinistefano <ste.vezzalini@gmail.com>
2025-01-06 12:34:58 +0100

    feat: configuring aliases

--- sh.nix
+++ sh.nix
@@ -1,13 +1,14 @@
 { config, pkgs, ... }:
 let
   myAliases = {
+    # git: basics
     gadd = "git add -v";
     gaddi = "git add --interactive";
     gcom = "git commit";
     gcomm = "git commit -m";
     gclone = "git clone";
     gcloneclip = "git clone $(getclip)";
-    ### logs;
+    # git: logs
     glog = "git log --all --decorate --oneline --graph";
     glog2 =
       "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
@@ -16,11 +17,11 @@ let
     gsh = "git show";
     gsta = "git status";
     gst = "git status";
-    ## Get updates;
+    # git: fetch updated
     gfa = "git fetch --all";
     gfap = "git fetch --all --prune --prune-tags";
     gpu = "git pull && git submodule update --checkout";
-    ### branching;
+    # git:branching
     gch = "git checkout";
     gchb = "git checkout -b";
     gbd = "git branch -d";
@@ -28,13 +29,31 @@ let
     gpd = "git push origin --delete";
     gmerg = "git merge --edit --stat --no-ff";
     gmerge = "git merge --edit --stat --no-ff";
-    ### submodules;
+    # git: submodules
     gsupc = "git submodule update --checkout";
-
+    # vim
     vim = "nvim";
-
+    # vim: git client
+    vimg = "nvim +G +only";
+    # vim: open current folder
+    e = "nvim .";
+    # cd ..
+    ".." = "cd ..";
+    # cd ../..
+    "..." = "cd ../..";
+    # cd ../../..
+    "...." = "cd ../../..";
+    # safer cp
+    cp = "cp -iv";
+    # safer rm
+    rm = "rm -Iv";
+    # safer mv
+    mv = "mv -Iv";
+    # nix: update nixos
     updateNix = "sudo nixos-rebuild switch --flake .#hostname";
+    # home-manager: update home
     updateHome = "home-manager switch --flake ~/.nix#sv";
+    # home-manager: update home and channel
     updateHomePlus =
       "sudo -i nix-channel --update && home-manager switch --flake ~/.nix#sv";
   };
